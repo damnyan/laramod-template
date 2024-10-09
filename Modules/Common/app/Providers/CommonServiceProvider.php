1295fd921557aad2824354b991f98af765a2c81d
dc <damnyan@gmail.com>
2024-10-09 21:50:24 +0800

    Add error handler

--- Modules/Common/app/Providers/CommonServiceProvider.php
+++ Modules/Common/app/Providers/CommonServiceProvider.php
@@ -25,6 +25,11 @@ class CommonServiceProvider extends ServiceProvider
         $this->registerConfig();
         // $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+
+        $this->app->singleton(
+            \Illuminate\Foundation\Exceptions\Handler::class,
+            fn ($app) => new \Dmn\Exceptions\Handler($app)
+        );
     }
 
     /**
